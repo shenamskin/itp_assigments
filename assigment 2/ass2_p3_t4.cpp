b01101ac3e998be459717630f94de047ea1e9955
shenamskin <altushkz2739@gmail.com>
2026-09-22 13:31:42 +0500

    assigment 2 part 3 task 4

---
+++ assigment 2/ass2_p3_t4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int number;
+
+    cin >> number;
+
+    for (int i = 1; i <= 10; i++){
+        cout << "\n" << number << " * " << i << " = " << number * i;
+    }
+    
+    
+}
